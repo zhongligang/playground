7a6e1d64d376ed5a0d8009b83b96b9970dcd40a5
zhongligang <zhongligang@gmail.com>
2011-11-30 22:03:52 +0800

    add ariprog

---
+++ usaco/ariprog.c
@@ -0,0 +1,12 @@
+/**
+ * Copyright (c) 2011 ZhongLigang. All Rights Reserved
+ * $Id$ 
+ * 
+ * @file ariprog.c
+ * @author zhongligang <ZhongLigang@gmail.com>
+ * @date 2011-11-30 22:03:14
+ */
+
+
+
+/* vim: set ts=4 sw=4 sts=4 tw=100 et: */
